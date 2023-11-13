1fd9b220f0223b4eb430527d5583c75efcc21888
bwilkins96 <ben.jr.speed@gmail.com>
2023-11-13 16:04:38 -0500

    goal model

--- goal_e_project/goal_e/models.py
+++ goal_e_project/goal_e/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Goal(models.Model):
+    """Goal model class"""
+
+    PRIORITY_CHOICES = [
+        (1, 'low'),
+        (2, 'medium'),
+        (3, 'high')
+    ]
+
+    title = models.CharField(max_length=65)
+    description = models.TextField(null=True, blank=True)
+    deadline = models.DateField()
+    progress = models.FloatField(default=0.0)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+    completed = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- goal_e_project/goal_e_project/settings.py
+++ goal_e_project/goal_e_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'goal_e.apps.GoalEConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
