8e4f2119cb5649575fd92927207665a8bf76971b
bwilkins96 <ben.jr.speed@gmail.com>
2023-12-04 12:32:00 -0500

    env file support

--- goal_e_project/goal_e_project/settings.py
+++ goal_e_project/goal_e_project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import dotenv_values
+
+# Environmental variables
+CONFIG = dotenv_values('.env')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aah3zcz#e+iq$z3v=(zh^1s--dy2173)_0hmm1fdp)u0*exjt('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if CONFIG.get('ENV') == 'prod':
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 
